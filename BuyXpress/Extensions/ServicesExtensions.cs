a45375609a1b3f88432aba268fbf09a5288b3409
Joshua Eze <ezejoshuac@gmail.com>
2023-07-21 17:16:48 +0100

    Injected Db connection service

--- BuyXpress/Extensions/ServicesExtensions.cs
+++ BuyXpress/Extensions/ServicesExtensions.cs
@@ -1,6 +1,15 @@
-﻿namespace BuyXpress.Extensions
+﻿using BuyXpress.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuyXpress.Extensions
 {
-    public class ServicesExtensions
+    public static class ServicesExtensions
     {
+        public static void AddDbConnection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BuyXpressDbContext>(option =>
+                option.UseSqlServer(configuration.GetConnectionString("DbConn"))
+            );
+        }
     }
 }
