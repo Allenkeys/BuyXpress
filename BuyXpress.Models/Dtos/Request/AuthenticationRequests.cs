46b6159d1350a82a196c81ae2d2f9ceb42be7961
Joshua Eze <ezejoshuac@gmail.com>
2023-07-24 17:28:53 +0100

    Modified Auth and User properties
    - User ID type set to string
    - Added signin and signup DTOs

---
+++ BuyXpress.Models/Dtos/Request/AuthenticationRequests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuyXpress.Models.Dtos.Request
+{
+    public class UserSignUpRequest
+    {
+        [Required]
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+    }
+
+    public class SignInRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
