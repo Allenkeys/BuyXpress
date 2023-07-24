97063cbfdeb33862213df9c4db82db64c5b597ac
Joshua Eze <ezejoshuac@gmail.com>
2023-07-24 19:38:35 +0100

    Set identity key type to string

--- BuyXpress.Data/Context/BuyXpressDbContext.cs
+++ BuyXpress.Data/Context/BuyXpressDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using BuyXpress.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BuyXpress.Data.Context
 {
-    public class BuyXpressDbContext : IdentityDbContext<ApplicationUser>
+    public class BuyXpressDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
         public BuyXpressDbContext(DbContextOptions<BuyXpressDbContext> options)
             : base(options) { }
