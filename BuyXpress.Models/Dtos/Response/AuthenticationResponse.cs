0b0b674b29e453431a8c3a5d4effa72164dd0300
Joshua Eze <ezejoshuac@gmail.com>
2023-07-27 13:06:59 +0100

    Added Authentication Responses DTOs
    - Created authenticated response dto
    - Created JwtToken response dto

--- BuyXpress.Models/Dtos/Response/AuthenticationResponse.cs
+++ BuyXpress.Models/Dtos/Response/AuthenticationResponse.cs
@@ -8,5 +8,17 @@ namespace BuyXpress.Models.Dtos.Response
 {
     public class AuthenticationResponse
     {
+        public JwtToken JwtToken { get; set; }
+        public string UserId { get; set; }
+
+        public string FullName { get; set; }
+        public string UserType { get; set; }
+    }
+
+    public class JwtToken
+    {
+        public string Token { get; set; }
+        public DateTime Issued { get; set; } = DateTime.Now;
+        public DateTime Expiration { get; set; }
     }
 }
