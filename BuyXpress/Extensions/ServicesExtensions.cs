b45aba9430fd078ee5148b94746fe03cedc655a5
Joshua Eze <ezejoshuac@gmail.com>
2023-07-28 15:46:29 +0100

    Configure JWT authentication middleware
    - Changed local host port

--- BuyXpress/Extensions/ServicesExtensions.cs
+++ BuyXpress/Extensions/ServicesExtensions.cs
@@ -1,10 +1,14 @@
-﻿using BuyXpress.Data.Context;
+﻿using System.Text;
+using BuyXpress.Data.Context;
 using BuyXpress.Data.Repository;
 using BuyXpress.Models.Entities;
 using BuyXpress.Services.Implementations;
+using BuyXpress.Services.Infrastructure.JWT;
 using BuyXpress.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BuyXpress.Extensions
 {
@@ -21,7 +25,32 @@ namespace BuyXpress.Extensions
         {
             services.AddScoped<IAuthenticationService, AuthenticationService>();
             services.AddScoped<IUnitOfWork, UnitOfWork<BuyXpressDbContext>>();
+            services.AddScoped<IJWTAuthenticator, JWTAuthenticator>();
             return services;
+        }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtConfig");
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings["Issuer"],
+                        ValidAudience = jwtSettings["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings["Key"]))
+                    };
+                });
         }
 
         public static void ConfigureIdentity(this IServiceCollection services)
